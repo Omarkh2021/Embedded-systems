bd094a4704a62e2d9916c424939d29835c3810d8
Omarkh2021 <90703502+Omarkh2021@users.noreply.github.com>
2023-08-28 14:36:03 +0300

    Update LCD.h

--- stepper_motor/LCD.h
+++ stepper_motor/LCD.h
@@ -9,39 +9,37 @@
 #define LCD_H_
 
 #define Clear_display 				 0x01
-	#define Return_home 				 0x02
-	#define Entry_mode_set 				 0x06
-	#define Display_OFF_Control		     0x08
-	#define Display_ON_Control		     0x0C
-	#define Cursor_or_Display_Shift		 0x80
-	#define lcd_FunctionReset  			 0x30
-	#define lcd_FunctionSet8bit 		 0x38â€�
+#define Return_home 				 0x02
+#define Entry_mode_set 				 0x06
+#define Display_OFF_Control		     0x08
+#define Display_ON_Control		     0x0C
+#define Cursor_or_Display_Shift		 0x80
+#define lcd_FunctionReset  			 0x30
+#define lcd_FunctionSet8bit 		 0x38
 
-#define LCD_DATA_PORT     PORTC
-#define LCD_CONTROL_PORT  PORTD
+#define PIN0    0
+#define PIN1    1
+#define PIN2    2
+#define PIN3    3
+#define PIN4    4
+#define PIN5    5
+#define PIN6    6
+#define PIN7    7
 
-#define PIN0   0
-#define PIN1   1
-#define PIN2   2
-#define PIN3   3
-#define PIN4   4
-#define PIN5   5
-#define PIN6   6
-#define PIN7   7
+#define LCD_Data_PORT 		PORTD
+#define LCD_CONTROL_PORT 	PORTB
 
-#define RS_PIN PIN0
-#define E_PIN  PIN2
-#define RW_PIN PIN1
+#define LCD_Data_PORT_DDR 		DDRD
+#define LCD_CONTROL_PORT_DDR 	DDRB
 
-#define LCD_DATA_PORT_DDR       DDRC
-#define LCD_CONTROL_PORT_DDR    DDRD
+#define RS_pin	PIN0
+#define RW_pin	PIN1
+#define E_pin	PIN2
 
-void LCD_VOID_SEND_DATA(u8 COPY_U8_DATA);
-
-void LCD_VOID_SEND_COMMAND(u8 COPY_U8_COMMAND);
-
-void LCD_VOID_INIT(void);
-
-void LCD_VOID_SET_CURSOR_LOCATION(u8 COPY_u8ROW,u8 COPY_U8COLUMN);
-
+void LCD_voidSendData(u8 copy_u8Data);
+void LCD_voidSendCommand(u8 copy_u8Command);
+void LCD_voidInti(void);
+void LCD_voidSetCursorLocation(u8 copy_u8Row,u8 copy_u8column);
+void LCD_voidWriteString(u8 * u8Ptrstring);
+void LCD_voidSaveCustomChar(u8 * u8PtrArr,u8 copy_u8Savelocation);
 #endif /* LCD_H_ */
