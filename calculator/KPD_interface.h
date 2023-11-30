09e19ce7ba43287a54deb80b3029a0db9b1e31de
Omarkh2021 <90703502+Omarkh2021@users.noreply.github.com>
2023-11-30 13:06:36 +0200

    Add files via upload

---
+++ calculator/KPD_interface.h
@@ -0,0 +1,14 @@
+/*
+ * KPD_interface.h
+ *
+ *  Created on: Sep 5, 2023
+ *      Author: bassant
+ */
+
+#ifndef KPD_INTERFACE_H_
+#define KPD_INTERFACE_H_
+
+void KPD_VidInit(void);
+u8 Kpd_U8GetPressedKey(void);
+
+#endif /* KPD_INTERFACE_H_ */
---
+++ calculator/LCD_config.h
@@ -0,0 +1,15 @@
+/*
+ * LCD_config.h
+ *
+ *  Created on: Sep 6, 2023
+ *      Author: engy
+ */
+
+#ifndef LCD_CONFIG_H_
+#define LCD_CONFIG_H_
+
+
+#define LCD_PORT	Port_A
+#define LCD_RS		Pin1
+#define LCD_E		Pin2
+#endif /* LCD_CONFIG_H_ */
